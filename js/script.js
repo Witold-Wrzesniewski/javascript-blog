b7a18501214cc72665d028367ef0b56be328e03c
Witold Wrześniewski <witold.wrzesniewski@poczta.fm>
2025-05-11 10:29:47 +0200

    Add 'event.preventDefault()'

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this
-    console.log('clickedElement (with plus): ' + clickedElement);
-    console.log('clickedElement:', clickedElement);
+    /*console.log('clickedElement (with plus): ' + clickedElement);
+    console.log('clickedElement:', clickedElement);*/
 
   /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -12,7 +13,7 @@ const titleClickHandler = function(event){
         activeLink.classList.remove('active');
     }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
     clickedElement.classList.add('active');
 
   /* [DONE] remove class 'active' from all articles */
